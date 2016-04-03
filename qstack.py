6a97354ea5eed485ecbc2331ad625a1de4c38f9e
Philip Youssef <philip.youssef@gmail.com>
2016-04-03 14:38:59 -0700

    got it working end2end for osx

--- qstack.py
+++ qstack.py
@@ -3,24 +3,24 @@ from src.scraper.xgoogle.search import GoogleSearch, SearchError
 from src.scraper.xoverflow import Xoverflow
 from src.rendering.webkit2png import WebkitRenderer
 from src.rendering.scripts import init_qtgui
+
 from PyQt4.QtCore import QTimer
 from PyQt4.QtWebKit import QWebSettings
 
 def main():
+    search_query = ' '.join(sys.argv[1:])
     stack_overflow_url = None
     try:
-        gs = GoogleSearch("stackoverflow.com: overflow error python")
+        gs = GoogleSearch("stackoverflow.com: {}".format(search_query))
         gs.results_per_page = 1
         results = gs.get_results()
         stack_overflow_url = [res.url.encode("utf8") for res in results][0]
     except SearchError, e:
         print "Search failed: %s" % e
-    
-    search_result = Xoverflow(stack_overflow_url).get_results()
-    #print search_result
-    
-    res = stack_overflow_url#(search_result, stack_overflow_url)
 
+    print stack_overflow_url
+    res = stack_overflow_url
+    
     def renderer_func():
         renderer = WebkitRenderer()
         renderer.width = 800
