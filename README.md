1dc4331a219fc75c0e36305283aac6311ca09a04
Philip Youssef <philip.youssef@gmail.com>
2016-04-03 16:30:37 -0700

    initial attempt to get phantom.js working

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+### Setup
+
+1. Install quartz for python: 
+2. Install requirements via pip: pip install -r requirements.txt
+3. Install X11 (if it isn't already present in environment
+> apt-get install ubuntu-desktop gdm vnc4server
--- qstack.py
+++ qstack.py
@@ -6,6 +6,7 @@ from src.rendering.scripts import init_qtgui
 
 from PyQt4.QtCore import QTimer
 from PyQt4.QtWebKit import QWebSettings
+from selenium import webdriver
 
 def main():
     search_query = ' '.join(sys.argv[1:])
@@ -19,26 +20,32 @@ def main():
         print "Search failed: %s" % e
 
     print stack_overflow_url
-    res = stack_overflow_url
-    
-    def renderer_func():
-        renderer = WebkitRenderer()
-        renderer.width = 800
-        renderer.height = 600
-        renderer.timeout = 10
-        renderer.wait = 1
-        renderer.format = "png"
-        renderer.grabWholeWindow = False
-        renderer.renderTransparentBackground = False
-        renderer.qWebSettings[QWebSettings.JavascriptEnabled] = True
-    
-        outfile = open("stackoverflow.png", "w")
-        renderer.render_to_file(res, outfile)
-        outfile.close()
 
-    app = init_qtgui()
-    QTimer.singleShot(0, renderer_func)
-    sys.exit(app.exec_())
+    driver = webdriver.PhantomJS() # or add to your PATH
+    driver.set_window_size(1024, 768) # optional
+    driver.get(stack_overflow_url)
+    driver.save_screenshot('screen.png') # save a screenshot to disk
+    # sbtn = driver.find_element_by_css_selector('button.gbqfba')
+    # sbtn.click()
+    
+    # def renderer_func():
+    #     renderer = WebkitRenderer()
+    #     renderer.width = 800
+    #     renderer.height = 600
+    #     renderer.timeout = 10
+    #     renderer.wait = 1
+    #     renderer.format = "png"
+    #     renderer.grabWholeWindow = False
+    #     renderer.renderTransparentBackground = False
+    #     renderer.qWebSettings[QWebSettings.JavascriptEnabled] = True
+    # 
+    #     outfile = open("stackoverflow.png", "w")
+    #     renderer.render_to_file(res, outfile)
+    #     outfile.close()
+    # 
+    # app = init_qtgui()
+    # QTimer.singleShot(0, renderer_func)
+    # sys.exit(app.exec_())
 
 if __name__ == '__main__':
     main()
